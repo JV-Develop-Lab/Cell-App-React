7f39ca93c42ccba0e83dc9e918cde559fc6bd893
jylee9937 <lkj49864@ajou.ac.kr>
2023-02-11 14:12:01 +0900

    :bug: build fail fix

--- src/pages/Book.tsx
+++ src/pages/Book.tsx
@@ -1,5 +1,4 @@
 import { ActivityComponentType } from "@stackflow/react";
-import { useStepFlow } from "../stackflow";
 import {Box} from "@chakra-ui/react";
 import PageLayout from "../components/layouts/PageLayout";
 
@@ -7,21 +6,11 @@ type ArticleParams = {
     title: string;
 };
 const Book: ActivityComponentType<ArticleParams> = ({ params }) => {
-    // 타입 안정성을 위해 현재 액티비티의 이름을 넣어줘요
-    const { stepPush } = useStepFlow("Article");
-    
-    const onNextClick = () => {
-        // `stepPush()`을 호출하면 params.title이 변경돼요.
-        stepPush({
-            title: "Next Title",
-        });
-    };
     
     return (
         <PageLayout>
             <Box height="100%">
                 <h1>{params.title}</h1>
-                <button onClick={onNextClick}>next</button>
             </Box>
             </PageLayout>
     );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,12 @@
 import { ActivityComponentType } from "@stackflow/react";
-import { useFlow } from "../stackflow";
 import PageLayout from "../components/layouts/PageLayout";
 import {Heading} from "@chakra-ui/react";
 
 const Home: ActivityComponentType = () => {
-    const { replace } = useFlow();
-    
-    const onClick = () => {
-        replace("Article", {
-            title: "Hello",
-        },{
-            animate: false
-        });
-    };
     
     return (
             <PageLayout>
                 <Heading>Howday 👋 갈렙!</Heading>
-                <button onClick={onClick}>Go to article page</button>
             </PageLayout>
     );
 };
